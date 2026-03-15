959188113f141239d254abac09aa95a6e545d27c
Ivan <237397488@qq.com>
2026-03-15 22:01:55 +0800

    test2

--- Formula/omlx.rb
+++ Formula/omlx.rb
@@ -1,7 +1,7 @@
 class Omlx < Formula
   desc "LLM inference server optimized for Apple Silicon"
   homepage "https://github.com/jundot/omlx"
-  url "https://github.com/jundot/omlx/archive/refs/tags/v0.2.12.tar.gz"
+  url "https://github.com/Edanflame/omlx/archive/refs/tags/0.2.15.tar.gz"
   sha256 "5744ad29522992dc4ee7297f26b9ee23ef7fcbbd68bf368d52b3f5c3312e846a"
   license "Apache-2.0"
 
